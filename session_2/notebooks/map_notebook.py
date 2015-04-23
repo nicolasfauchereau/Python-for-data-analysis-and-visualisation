fa0749933c402f318fd28d1d6f733bd002c9913e
Nicolas Fauchereau <nicolas.fauchereau@gmail.com>
2015-04-23 16:41:41 +1200

    added map_notebook.py

---
+++ session_2/notebooks/map_notebook.py
@@ -0,0 +1,22 @@
+from IPython.display import HTML
+import folium
+
+def inline_map(map):
+    """
+    Embeds the HTML source of the map directly into the IPython notebook.
+
+    This method will not work if the map depends on any files (json data). Also this uses
+    the HTML5 srcdoc attribute, which may not be supported in all browsers.
+    """
+    map._build_map()
+    return HTML('<iframe srcdoc="{srcdoc}" style="width: 100%; height: 510px; border: none"></iframe>'.format(srcdoc=map.HTML.replace('"', '&quot;')))
+
+def embed_map(map, path="map.html"):
+    """
+    Embeds a linked iframe to the map into the IPython notebook.
+
+    Note: this method will not capture the source of the map into the notebook.
+    This method should work for all maps (as long as they use relative urls).
+    """
+    map.create_map(path=path)
+    return HTML('<iframe src="files/{path}" style="width: 100%; height: 510px; border: 3"></iframe>'.format(path=path))
